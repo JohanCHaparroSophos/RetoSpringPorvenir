636ef18c50670584f7c0e1e64dd59b7e867fd895
Johan Chaparro <johan.chaparro@sophossolutions.com>
2023-12-01 13:45:05 -0500

    Creación del proyecto

---
+++ src/test/java/com/porvenir/retospring/web/app/RetoSpringPorvenirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porvenir.retospring.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoSpringPorvenirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
